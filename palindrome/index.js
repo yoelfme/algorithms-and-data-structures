a83150baf678f166e363307934ea04113655ef93
Yoel Monzon <yoelfme@hotmail.com>
2018-05-21 17:13:48 -0500

    [add] solution #2 for palindrome exercise

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,3 +1,10 @@
-module.exports = (str) => {
+const palindrome1 = (str) => {
   return str === str.split('').reverse().join('')
+}
+
+module.exports = (str) => {
+  return str.split('')
+            .every((character, i) => {
+              return character === str[str.length - i - 1]
+            })
 }
