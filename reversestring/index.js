e15ece9b73091193d0ac6df40120d4040ba31a29
Yoel Monzon <yoelfme@hotmail.com>
2018-05-21 15:12:12 -0500

    [add] solution #2 and #3 for reverse engineering

--- reversestring/index.js
+++ reversestring/index.js
@@ -1,1 +1,15 @@
-module.exports = (str) => str.split('').reverse().join('')
+const reverse1 = (str) => str.split('').reverse().join('')
+const reverse2 = (str) => {
+    let reversed = ''
+
+    for (let character of str) {
+        reversed = `${character}${reversed}`
+    }
+    
+    return reversed
+}
+
+
+module.exports = (str) => {
+    return str.split('').reduce((rev, char) => char +  rev, '')
+}
