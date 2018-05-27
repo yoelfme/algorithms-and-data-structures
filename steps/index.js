51d2a5284ed7358f446883ecaa3095801cbf59be
Yoel Monzon <yoelfme@hotmail.com>
2018-05-27 18:27:37 -0500

    [add] solution #2 for steps problem

--- steps/index.js
+++ steps/index.js
@@ -1,6 +1,18 @@
-module.exports = (steps) => {
+const steps1 = (steps) => {
   for (let i = 1; i <= steps; i++) {
     const line = Array(i).fill('#').concat(Array(steps-i).fill(' ')).join('')
     console.log(line)
+  }
+}
+
+module.exports = (steps) => {
+  for (let row = 0; row < steps; row++) {
+    let stair = ''
+
+    for (let column = 0; column < steps; column++) {
+      stair += (column <= row) ? '#': ' '
+    }
+
+    console.log(stair)
   }
 }
