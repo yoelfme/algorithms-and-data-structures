e7464926cd1fc8ea3ba15b84ec1bd247308cfbb7
Yoel Monzon <yoelfme@hotmail.com>
2018-05-27 17:49:15 -0500

    [update] solution #2 for capitalize problem

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,6 +1,15 @@
-module.exports = (string) => {
-
+const capitalize1 = (string) => {
   return string.split(' ')
     .map((word) => `${word[0].toUpperCase()}${word.slice(1)}`)
     .join(' ')
+}
+
+module.exports = (string) => {
+  const words = []
+
+  for (let word of string.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1))
+  }
+
+  return words.join(' ')
 }
