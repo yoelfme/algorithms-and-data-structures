077375205c88ff69cddc15e2d55b748f5fd6cdf0
Yoel Monzon <yoelfme@hotmail.com>
2018-05-29 15:29:16 -0500

    [add] solution #1 for problem matrix

---
+++ matrix/index.js
@@ -0,0 +1,45 @@
+module.exports = (n) => {
+  const matrix = []
+  const elements = n * n
+  let startColumn = 0
+  let endColumn = n - 1
+  let startRow = 0
+  let endRow = n - 1
+  let row = 0
+  let counter = 1
+
+  for (let i = 0; i < n; i++) {
+    matrix[i] = []
+  }
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      matrix[startRow][i] = counter++
+    }
+
+    startRow++
+
+
+    for (let i = startRow; i <= endRow; i++) {
+      matrix[i][endColumn] = counter++
+    }
+
+    endColumn--
+
+    for (let i = endColumn; i >= startColumn; i--) {
+      matrix[endRow][i] = counter++
+    }
+
+    endRow--
+
+    for (let i = endRow; i >= startRow; i--) {
+      matrix[i][startColumn] = counter++
+    }
+
+    startColumn++
+  }
+
+  console.log(matrix)
+
+  return matrix
+ }
