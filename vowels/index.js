ba4c68fe9b0b7542755df59719cda65ee4a5d2e8
Yoel Monzon <yoelfme@hotmail.com>
2018-05-28 19:58:21 -0500

    [add] solution #2 for vowels problem

--- vowels/index.js
+++ vowels/index.js
@@ -1,5 +1,18 @@
-module.exports = (str) => {
+const vowels1 = (str) => {
   const vowels = str.toLowerCase().match(/[aeiou]/g)
 
   return vowels && vowels.length || 0
+}
+
+module.exports = (str) => {
+  let count = 0
+  const checker = ['a', 'e', 'i', 'o', 'u']
+
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      count++
+    }
+  }
+
+  return count
 }
