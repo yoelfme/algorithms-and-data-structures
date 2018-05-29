e383fd4e5eb76e37f35ba8fdced8186985ccb938
Yoel Monzon <yoelfme@hotmail.com>
2018-05-28 19:16:04 -0500

    [add] recursive solution for pyramid problem

--- pyramid/index.js
+++ pyramid/index.js
@@ -1,4 +1,4 @@
-module.exports = (level) => {
+const pyramid1 = (level) => {
   const columns = (level * 2) - 1
   const delta = Math.floor(columns / 2)
 
@@ -12,4 +12,22 @@ module.exports = (level) => {
     }
     console.log(stair)
   }
+}
+
+module.exports = function pyramid(levels, row = 0, level = '') {
+  if (levels === row)  return;
+
+  const columns = (levels * 2) - 1
+  const midpoint = Math.floor(columns / 2)
+  const max = midpoint + row
+  const min = midpoint - row
+
+  if (columns === level.length) {
+    console.log(level)
+    return pyramid(levels, row + 1)
+  }
+
+  const add = (level.length >= min && level.length <= max) ? '#' : ' '
+
+  return pyramid(levels, row, level + add)
 }
