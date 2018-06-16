9ec0fc2415403b8dd24da853dae7aa30c3db65bc
Yoel Monzon <yoelfme@hotmail.com>
2018-06-16 14:39:10 -0500

    [add] implementation for size, getFirst and getLast function in linked list class

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -18,6 +18,35 @@ class LinkedList {
     const node = new Node(data, this.head)
     this.head = node
   }
+
+  size() {
+    let node = this.head
+    let size = 0
+
+    while (node) {
+      size += 1
+
+      node = node.next
+    }
+
+    return size
+  }
+
+  getFirst() {
+    return this.head
+  }
+
+  getLast() {
+    let last = this.head
+
+    if (!last) return null
+
+    while (last.next) {
+      last = last.next
+    }
+
+    return last
+  }
 }
 
 module.exports = { Node, LinkedList };
--- linkedlist/index.test.js
+++ linkedlist/index.test.js
@@ -28,7 +28,7 @@ describe('Insert First', () => {
   });
 });
 
-describe.skip('Size', () => {
+describe('Size', () => {
   test('returns the number of items in the linked list', () => {
     const l = new List();
     expect(l.size()).toEqual(0);
@@ -40,7 +40,7 @@ describe.skip('Size', () => {
   });
 });
 
-describe.skip('GetFirst', () => {
+describe('GetFirst', () => {
   test('returns the first element', () => {
     const l = new List();
     l.insertFirst(1);
@@ -50,9 +50,10 @@ describe.skip('GetFirst', () => {
   });
 });
 
-describe.skip('GetLast', () => {
+describe('GetLast', () => {
   test('returns the last element', () => {
     const l = new List();
+    expect(l.getLast()).toEqual(null)
     l.insertFirst(2);
     expect(l.getLast()).toEqual({ data: 2, next: null });
     l.insertFirst(1);
