7b88939acf88371edc81cfb719bf0a6555a21e1f
Yoel Monzon <yoelfme@hotmail.com>
2018-06-21 16:17:37 -0500

    [add] solution for buble sort algorithm

---
+++ sorting/index.js
@@ -0,0 +1,32 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      const element = arr[j]
+      const next = j+1
+
+      if (element > arr[next]) {
+        arr[j] = arr[next]
+        arr[next] = element
+      }
+    }
+  }
+
+  return arr
+}
+
+function selectionSort(arr) {
+
+}
+
+function mergeSort(arr) {
+
+}
+
+function merge(left, right) {
+
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort };
